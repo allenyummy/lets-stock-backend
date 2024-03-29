50b57d5e28b3ba2b46cfe852ef59eea3ed7dfc8f
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2024-03-29 10:50:09 +0800

    feat: add stock service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,8 @@
+import { stock } from './stock/stock'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
+  app.configure(stock)
   // All services will be registered here
 }
